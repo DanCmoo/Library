abe1d7cb04d2acfaaed913f9a20615d3c52711a9
DanCmo <dankurso845@gmail.com>
2024-11-07 18:54:47 -0500

    Initial commit

---
+++ src/main/java/com/dancmo/project/libraryud/LibraryUdApplication.java
@@ -0,0 +1,13 @@
+package com.dancmo.project.libraryud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryUdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryUdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dancmo/project/libraryud/LibraryUdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dancmo.project.libraryud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryUdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
