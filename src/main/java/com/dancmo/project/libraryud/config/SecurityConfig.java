09f27c3b911639a0d36e2f20e181b53ffb39bbc4
DanCmo <dankurso845@gmail.com>
2024-11-14 15:56:34 -0500

    [feat]: se implementa la dependencia de security

---
+++ src/main/java/com/dancmo/project/libraryud/config/SecurityConfig.java
@@ -0,0 +1,11 @@
+package com.dancmo.project.libraryud.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+
+}
